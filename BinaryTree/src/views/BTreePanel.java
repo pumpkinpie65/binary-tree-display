ca635826746b133ff3bc09be1e4cd2e1b9eea5e7
pumpkinpie65 <pumpkinpie65@live.com>
2014-05-08 10:47:17 -0700

    started on painting the tree to screen

--- BinaryTree/src/views/BTreePanel.java
+++ BinaryTree/src/views/BTreePanel.java
@@ -29,5 +29,29 @@ public class BTreePanel extends JPanel {
         /*   UNCOMMENT WHEN YOU HAVE THE GUI TREE CODE
          theTree.displayTree(g,theTree.getRoot(),WINWIDTH/2,80, 1);
         */
+         
+         //g.drawOval(getWidth() / 2, getHeight() - (getHeight() - 10), 20, 20);
+         int x = getWidth() / 2;
+         int y =  getHeight() - (getHeight() - 20);
+         int diameter = 20;
+         drawCenteredCircle(g, x, y, diameter);
+         drawLeftChildLine(g, x, y);
+         drawRightChildLine(g, x, y);
+  }
+  
+  public void drawCenteredCircle(Graphics g, int x, int y, int d) {
+        x = x-(d/2);
+        y = y-(d/2);
+        g.drawOval(x,y,d,d);
+    }
+  
+  public void drawLeftChildLine(Graphics g, int circleCenterX, int circleCenterY)
+  {
+      g.drawLine(circleCenterX, circleCenterY, circleCenterX - (getWidth() / 4), circleCenterY + (getHeight() / 4));
+  }
+  
+   public void drawRightChildLine(Graphics g, int circleCenterX, int circleCenterY)
+  {
+      g.drawLine(circleCenterX, circleCenterY, circleCenterX + (getWidth() / 4), circleCenterY + (getHeight() / 4));
   }
 }
